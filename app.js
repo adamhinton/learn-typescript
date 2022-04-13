e4829e0fffbb41e46fee18a8cdc705595b645244
adamhinton <adam.d.hinton@gmail.com>
2022-04-13 13:52:14 -0400

    learn about truthy/falsy values

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-function add(n1, n2) {
+function add(n1, n2, showResult) {
     console.log(typeof number1);
-    return n1 + n2;
+    if (showResult) {
+        console.log(n1 + n2);
+    }
+    else {
+        return n1 + n2;
+    }
 }
-var number1 = 5;
+//important note: All types in TS (number, string etc) are ALWAYS lower case!
+var number1 = 5; //same data type as e.g. 5.0
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var result = add(number1, number2, printResult);
+// console.log(result);
--- app.ts
+++ app.ts
@@ -1,12 +1,16 @@
-function add(n1: number, n2: number) {
-  console.log(typeof number1);
-  return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean) {
+  if (showResult) {
+    console.log(n1 + n2);
+  } else {
+    return n1 + n2;
+  }
 }
 
 //important note: All types in TS (number, string etc) are ALWAYS lower case!
 
-const number1 = 5;
+const number1 = 5; //same data type as e.g. 5.0
 const number2 = 2.8;
+const printResult = true;
 
-const result = add(number1, number2);
-console.log(result);
+const result = add(number1, number2, printResult);
+// console.log(result);
