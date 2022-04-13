5997a637a23428d4342922d52a8837464295e104
adamhinton <adam.d.hinton@gmail.com>
2022-04-13 17:39:41 -0400

    learn type arrays

--- app.ts
+++ app.ts
@@ -1,6 +1,14 @@
 const person = {
   name: "Maximilian",
   age: 30,
+  hobbies: ["Sports", "cooking"],
 };
 
+let favActivities: string[];
+favActivities = ["Sports"];
+
 console.log(person);
+
+for (const hobby of person.hobbies) {
+  console.log("hobby:", hobby);
+}
