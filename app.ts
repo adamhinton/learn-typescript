1541a30e3ffe76a3401992b26659d253d3cbc9b7
adamhinton <adam.d.hinton@gmail.com>
2022-04-13 13:47:33 -0400

    helpful comments

--- app.ts
+++ app.ts
@@ -3,6 +3,8 @@ function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
+//important note: All types in TS (number, string etc) are ALWAYS lower case!
+
 const number1 = 5;
 const number2 = 2.8;
 
