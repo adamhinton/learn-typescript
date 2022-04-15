4a82307a92d6e85d315fa1bbc4bcc4cffe2edc2d
adamhinton <adam.d.hinton@gmail.com>
2022-04-15 00:33:19 -0400

    define addition fxn

--- app.ts
+++ app.ts
@@ -1,27 +1,4 @@
-// const person = {
-//   name: "Max",
-//   age: 30,
-//   hobbies: ["sports"],
-// };
-
-enum Role {
-  ADMIN,
-  READ_ONLY,
-  AUTHOR,
-}
-
-const person = {
-  name: "Max",
-  age: 30,
-  hobbies: ["sports"],
-  role: 0,
+const add = (n1: number, n2: number) => {
+  const result = n1 + n2;
+  return result;
 };
-
-let favAct: string[];
-
-for (const hobby of person.hobbies) {
-  console.log(hobby);
-}
-
-//tuple: fixed length, fixed type
-//enum: automatically generated global constant identifiers
