2c3c9346772bcd2e4ed8dce0b12d9b1c4d6032a1
adamhinton <adam.d.hinton@gmail.com>
2022-04-15 16:58:57 -0400

    make first return type

--- app.ts
+++ app.ts
@@ -1,6 +1,12 @@
 //learning return types
 //TS knows that this should return a number
 
-const add = (n1: number, n2: number){
-    return n1 + n2;
-}
+// const add = (n1: number, n2: number) =>{
+//     return n1 + n2;
+// }
+
+//can explicitly assign a return type by writing :string (or whatever type) after the parameter list, like so:
+// const add = (n1: number, n2: number): string => {
+//   return String(n1 + n2);
+// };
+//now TS knows this should return a string. Would give an error if it were just return n1 + n2;
