d0573ce301290a35125e0fb775b95815039d7b10
adamhinton <adam.d.hinton@gmail.com>
2022-04-15 16:34:35 -0400

    adding app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var add = function (input1, input2) {
+var add = function (input1, input2, resultConversion) {
     var result;
     if (typeof input1 === "number" && typeof input2 === "number") {
         result = input1 + input2;
@@ -6,7 +6,14 @@ var add = function (input1, input2) {
     else {
         result = String(input1) + String(input2);
     }
+    if (resultConversion === "as-number") {
+        return +result;
+    }
+    else {
+        return String(result);
+    }
     return result;
 };
-console.log(add("Max", "Anna"));
-console.log(add(434, 543));
+console.log(add("Max", "Anna", "as-text"));
+console.log(add(434, 543, "as-number"));
+console.log(add("456456", "832742", "as-number"));
