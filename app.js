479ae78935aaf43441a41e6700fb54854ddb7a91
adamhinton <adam.d.hinton@gmail.com>
2022-04-15 00:40:01 -0400

    define my first union

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var person = {
-    name: "Maximilian",
-    age: 30,
-    hobbies: ["Sports", "cooking"]
+var add = function (input1, input2) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = String(input1) + String(input2);
+    }
+    return result;
 };
-var favActivities;
-favActivities = ["Sports"];
-console.log(person);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log("hobby:", hobby);
-}
+console.log(add("Max", "Anna"));
+console.log(add(434, 543));
--- app.ts
+++ app.ts
@@ -1,4 +1,15 @@
-const add = (n1: number, n2: number) => {
-  const result = n1 + n2;
+const add = (
+  input1: number | string | boolean,
+  input2: number | string | boolean
+) => {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = String(input1) + String(input2);
+  }
   return result;
 };
+
+console.log(add("Max", "Anna"));
+console.log(add(434, 543));
