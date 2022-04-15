5998c947497dae4e0b40d5e145a704e941e4858d
adamhinton <adam.d.hinton@gmail.com>
2022-04-15 16:55:29 -0400

    rename app to union-aliases, create app.ts

--- app.ts
+++
@@ -1,9 +0,0 @@
-type NumString = number | string;
-
-const combine = (n1: NumString, n2: NumString) => {
-  const result = n1 + n2;
-  return result;
-};
-
-const combinedAges = combine("32492", "392432");
-console.log(combinedAges);
