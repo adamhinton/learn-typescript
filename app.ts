aa64d8f3e74976e890ab901b240f94dfce128a31
adamhinton <adam.d.hinton@gmail.com>
2022-04-15 00:15:20 -0400

    define a tuple again

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ const person = {
   name: "Max",
   age: 30,
   hobbies: ["sports"],
+  role: [number, string],
 };
 
 let favAct: string[];
@@ -9,3 +10,5 @@ let favAct: string[];
 for (const hobby of person.hobbies) {
   console.log(hobby);
 }
+
+//tuple: fixed length, fixed type
