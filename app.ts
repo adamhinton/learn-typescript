2026c8de8d4b5d0953609192c8029601554c2e10
adamhinton <adam.d.hinton@gmail.com>
2022-04-15 16:34:27 -0400

    learned custom types and aliases

--- app.ts
+++ app.ts
@@ -1,17 +1,35 @@
-const add = (
-  input1: number | string,
-  input2: number | string,
-  resultConversion: string
-) => {
-  let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
-  } else {
-    result = String(input1) + String(input2);
-  }
-  return result;
+// type Combinable = number | string;
+// type ConversionDescriptor = "as-number" | "as-text";
+
+// const add = (
+//   input1: number | string,
+//   input2: number | string,
+//   resultConversion: ConversionDescriptor
+// ) => {
+//   let result;
+//   if (typeof input1 === "number" && typeof input2 === "number") {
+//     result = input1 + input2;
+//   } else {
+//     result = String(input1) + String(input2);
+//   }
+//   if (resultConversion === "as-number") {
+//     return +result;
+//   } else {
+//     return String(result);
+//   }
+//   return result;
+// };
+
+// console.log(add("Max", "Anna", "as-text"));
+// console.log(add(434, 543, "as-number"));
+// console.log(add("456456", "832742", "as-number"));
+
+type User = { name: string; age: number };
+
+const greet = (user: User) => {
+  console.log(`Hi, I am ${user.name}`);
 };
 
-console.log(add("Max", "Anna", "as-text"));
-console.log(add(434, 543, "as-number"));
-console.log(add("456456", "832742", "as-number"));
+const isOlder = (user: User, checkAge: number) => {
+  return checkAge > user.age;
+};
