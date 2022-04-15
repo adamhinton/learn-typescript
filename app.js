f54ebf2cfd506580ed8f8402e70db1d2b6ddfb1c
adamhinton <adam.d.hinton@gmail.com>
2022-04-15 17:01:09 -0400

    void notes

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-var combine = function (n1, n2) {
-    var result = n1 + n2;
-    return result;
+//learning return types
+//TS knows that this should return a number
+var add = function (n1, n2) {
+    return n1 + n2;
 };
-var combinedAges = combine("32492", "392432");
-console.log(combinedAges);
+//can explicitly assign a return type by writing :string (or whatever type) after the parameter list, like so:
+// const add = (n1: number, n2: number): string => {
+//   return String(n1 + n2);
+// };
+//now TS knows this should return a string. Would give an error if it were just return n1 + n2;
+//But it's best to let TS just infer the type unless you need to do otherwise.
+//void types:
+var printRes = function (num) {
+    console.log("Result: " + num);
+};
+printRes(add(5, 12));
--- app.ts
+++ app.ts
@@ -1,12 +1,22 @@
 //learning return types
 //TS knows that this should return a number
 
-// const add = (n1: number, n2: number) =>{
-//     return n1 + n2;
-// }
+const add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
 
 //can explicitly assign a return type by writing :string (or whatever type) after the parameter list, like so:
 // const add = (n1: number, n2: number): string => {
 //   return String(n1 + n2);
 // };
 //now TS knows this should return a string. Would give an error if it were just return n1 + n2;
+
+//But it's best to let TS just infer the type unless you need to do otherwise.
+
+//void types:
+//This return type is a void, it doesn't have a return statement.
+const printRes = (num: number) => {
+  console.log("Result: " + num);
+};
+
+printRes(add(5, 12));
