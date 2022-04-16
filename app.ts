8063b8042e46df5fcebe1c2bb27fcc3e1398e119
adamhinton <adam.d.hinton@gmail.com>
2022-04-15 22:35:30 -0400

    create assignment error on purpose

--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
 let userInput: unknown;
+//can store any value in this without getting errors
+userInput = 5;
+userInput = "Max";
+
+//unknown is different from any.
+//The below would return an error because type 'unknown' is not assignable to type 'string'
+let userName: string;
+userName = userInput;
