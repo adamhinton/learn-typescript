26c2d778f1ff1bea77de2403a21b1883d7a60b5e
adamhinton <adam.d.hinton@gmail.com>
2022-04-19 21:31:52 -0400

    rename file to app.ts

---
+++ app.ts
@@ -0,0 +1,86 @@
+console.log("afjidoasfs");
+
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+//You create an intersection type like so
+//This is a combination of the properties of Admin and Employee
+type ElevatedEmployee = Admin & Employee;
+
+//here we declare an ElevatedEmployee
+const el: ElevatedEmployee = {
+  name: "Max",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
+
+//intersections are useful with objects but you can use them with any types:
+type Combinable = string | number;
+type Numeric = number | boolean;
+type Universal = Combinable & Numeric;
+
+//my first type guard:
+//I check here what type the arguments are and return something based on that
+const add = (a: Combinable, b: Combinable) => {
+  if (typeof a === "string" || typeof b === "string") {
+    return String(a) + String(b);
+  }
+  return a + b;
+};
+
+type UnknownEmployee = Employee | Admin;
+
+//'privileges' in emp below is a nift way to access properties for type guards
+const printEmployeeInfo = (emp: UnknownEmployee) => {
+  console.log("emp.name:", emp.name);
+  if ("privileges" in emp) {
+    console.log("emp.privileges:", emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("emp.startDate:", emp.startDate);
+  }
+};
+
+printEmployeeInfo(el);
+
+//starting Discriminated Unions
+//we add a type key (or any other key) to each interface here,
+//then use a switch statement in moveAnimal function based on the interface's type
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+const moveAnimal = (animal: Animal) => {
+  // console.log("Moving with speed:" + animal.flyingSpeed);
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+
+    case "horse":
+      speed = animal.runningSpeed;
+      break;
+
+    default:
+      null;
+  }
+  console.log(`Moving at speed: ${speed}`);
+};
+
+moveAnimal({ type: "bird", flyingSpeed: 50 });
