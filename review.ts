be7436ba8c7dce3c13d40270ff711387d6e4c8d4
adamhinton <adam.d.hinton@gmail.com>
2022-04-22 17:54:29 -0400

    make intersction type

--- review.ts
+++ review.ts
@@ -1,1 +1,14 @@
 //INTERSECTION TYPE REVIEW
+//allow us to combine other types
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+//Now we create our intersection of thes two:
+type ElevatedEmployee = Admin & Employee;
