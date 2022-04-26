ea60b8c9155ca019b1961a1ba3db830bfcda0228
adamhinton <adam.d.hinton@gmail.com>
2022-04-25 22:22:20 -0400

    correct typo

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 //arrays are a generic. They require a type argument of sorts.
 //they're connected with another type
-
 // const names: Array<string> = [];
-
 // //now TS knows names[0] is a string
 // names[0].split(" ");
-
 // //The Promise type is another generic
 // //This promise eventually returns a string
 // const promise: Promise<string> = new Promise((resolve, reject) => {
@@ -13,12 +10,11 @@
 //         resolve("this is done!");
 //     }), 2000)
 // });
-
 //Now to build our own generic types
 //This fxn returns intersection of T and U
 //This tells TS that T and U are different items so can be merged.
-function merge<T, U>(objA: T, objB: U) {
-  return Object.assign(objA, objB);
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
 }
-const mergedObj = merge({ name: "Max" }, { age: 30 });
+var mergedObj = merge({ name: "Max" }, { age: 30 });
 console.log(mergedObj.age);
