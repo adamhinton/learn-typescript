c6a717eee35eff1615c933ec25ddd5a2a7463dc5
adamhinton <adam.d.hinton@gmail.com>
2022-04-25 22:08:14 -0400

    more generic notes: they're combined with another type

--- app.ts
+++ app.ts
@@ -1,3 +1,7 @@
 //arrays are a generic. They require a type argument of sorts.
+//they're connected with another type
 
-const names: Array = [];
+const names: Array<string> = [];
+
+//now TS knows this is a string
+names[0];
