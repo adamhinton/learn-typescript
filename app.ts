a79066eca449ec42eb7d18829529999db012803d
adamhinton <adam.d.hinton@gmail.com>
2022-04-25 22:15:29 -0400

    Now to build our own generic types

--- app.ts
+++ app.ts
@@ -1,15 +1,17 @@
 //arrays are a generic. They require a type argument of sorts.
 //they're connected with another type
 
-const names: Array<string> = [];
+// const names: Array<string> = [];
 
-//now TS knows names[0] is a string
-names[0].split(" ");
+// //now TS knows names[0] is a string
+// names[0].split(" ");
 
-//The Promise type is another generic
-//This promise eventually returns a string
-const promise: Promise<string> = new Promise((resolve, reject) => {
-    setTimeout(() =>{
-        resolve("this is done!");
-    }), 2000)
-});
+// //The Promise type is another generic
+// //This promise eventually returns a string
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//     setTimeout(() =>{
+//         resolve("this is done!");
+//     }), 2000)
+// });
+
+//Now to build our own generic types
