81ef5e1ab1df30fbee5fff47365bad8dc4c91715
adamhinton <adam.d.hinton@gmail.com>
2022-04-25 22:29:00 -0400

    notes on what constraints do

--- app.ts
+++ app.ts
@@ -19,10 +19,12 @@
 //Now to build our own generic types
 //This fxn returns intersection of T and U
 //This tells TS that T and U are different items so can be merged.
-function merge<T, U>(objA: T, objB: U) {
-  return Object.assign(objA, objB);
-}
-const mergedObj = merge({ name: "Max" }, { age: 30 });
-console.log(mergedObj.age);
+// function merge<T, U>(objA: T, objB: U) {
+//   return Object.assign(objA, objB);
+// }
+// const mergedObj = merge({ name: "Max" }, { age: 30 });
+// console.log(mergedObj.age);
 
 //CONSTRAINTS:
+//Here we will do much the same as above, defining a generic merge() function that merges two objects
+//we take it a step further by adding constraints, telling TS that both parameters are objects
